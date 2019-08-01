d6571504600f6e0b4c0d9e4e07fee47cf26c1f2c
duwei <duwei19961021@163.com>
2019-08-02 01:07:09 +0800

    Initial commit

---
+++ gofirst/project1/main/helloworld.go
@@ -0,0 +1,34 @@
+package main
+
+/*func main()  {
+	fmt.Println("hello word","duwei")
+}
+go build helloworld.go 生成helloworld.exe二进制文件
+常用转义字符：\t,\n,\\,\",\r回车*/
+
+/*func main()  {
+	fmt.Println("hello\tworld")
+	fmt.Println("hello\nworld")
+	fmt.Println("hello\\world")
+	fmt.Println("hello\"world")
+	fmt.Println("helloddddd\rworld")
+}*/
+
+/*func main()  {
+	fmt.Println("hello world")
+}*/
+
+import "fmt"
+
+func main()  {
+	//定义变量
+	var i int
+	//i = 20
+	//使用变量
+	fmt.Println(i)
+	/*
+	使用变量的三种方式：
+	1、指定变量类型，声明后若不赋值，则使用默认值
+	2、
+	 */
+}
