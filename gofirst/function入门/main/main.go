d0b94fe9e0ec0ebc72cb09958e1b664ec8ac6c05
duwei <duwei19961021@163.com>
2019-08-09 02:05:25 +0800

    2019/08/09 02:04

--- gofirst/function入门/main/main.go
+++ gofirst/function入门/main/main.go
@@ -1,12 +1,44 @@
 package main
 
-import "fmt"
+//import (
+//	"cal"
+//	"fmt"
+//)
+//func main() {
+//	fmt.Println(cal.Cal(10,20))
+//}
 
-func cal(n1 float64, n2 float64) (sum float64) {
-	sum = n2 + n1
+//import "fmt"
+//func test(n1 int64) {
+//	if n1 > 10 {
+//		n1--
+//		test(n1)
+//	}
+//	fmt.Println(n1)
+//}
+//
+//func main() {
+//	test(64)
+//}
+
+import (
+	"fmt"
+)
+
+func test(last int64) (sum int64) {
+	count := 0
+	count++
+	if count < 10 {
+		sum := (last + 1) * 2
+		count++
+		test(last)
+	}
 	return sum
 }
+
 func main() {
-	result := cal(10, 20)
-	fmt.Println(result)
+	nice := test(1)
+	fmt.Println(nice)
 }
+
+//1 4  (n+1)2
---
+++ src/cal/cal.go
@@ -0,0 +1,6 @@
+package cal
+
+func Cal(n1 int64, n2 int64) (sum int64) {
+	sum = n1 + n2
+	return sum
+}
