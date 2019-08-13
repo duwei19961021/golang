2c67c73bd17384287856de654cd7ab0eef0f2a41
duwei work <duwei19961021@163.com>
2019-08-13 18:00:08 +0800

    2019/08/12 18:00

--- gofirst/function入门/函数/error/main.go
+++ gofirst/function入门/函数/error/main.go
@@ -107,3 +107,87 @@ func main() {
 //	mp(&arr)
 //	fmt.Println(arr)
 //}
+
+//二分
+//func binary(arr *[11]int,Leftindex int,Rightindex int,num int){
+//	middle := (Leftindex+Rightindex)/2
+//}
+//func main() {
+//	arr := [11]int{12,14,16,19,20,25,29,30,33,39,40}
+//	binary(&arr,leftindex)
+//}
+
+//二位数组
+//func main() {
+//	var arr [4][6]int
+//	arr [1][2]  = 1
+//	for i:=0;i < 4;i++{
+//		for j:=0;j < 6;j++{
+//			fmt.Print(arr[i][j]," ")
+//		}
+//		fmt.Println()
+//	}
+//}
+
+//func main() {
+//var arr [4][6]int = [4][6]int{{1,2,3,4,5,6},{1,2,3,4,5,6},{1,2,3,4,5,6},{1,2,3,4,5,6}}
+
+//fmt.Println(arr)
+//for i := 0 ;i < 4;i++ {
+//	for j := 0; j < 6; j++ {
+//		fmt.Print(arr[i][j]," ")
+//	}
+//	fmt.Println()
+//}
+
+//for index,value := range arr{
+//	for j,v2 := range value{
+//		fmt.Printf("arr[%v][%v]=[%v]\n",index,j,v2)
+//	}
+//}
+//}
+
+//func main() {
+//	var arr [3][5]int
+//	for i,v1 := range arr{
+//		for j,_ := range v1{
+//			fmt.Printf("请输入第%d个班第%d位:",i+1,j+1)
+//			fmt.Scanln(&arr[i][j])
+//		}
+//	}
+//	fmt.Println(arr)
+//}
+
+//map声明
+//func main() {
+//	var a map[string]string //使用前必须给map分配数据空间
+//	a = make(map[string]string,10) //10表示分配给map的空间，能存10个key-value
+//	a["name"] = "dw" //value可重复，key不可重复(覆盖先前的)
+//	a["age"] = "18"
+//	a["hoby"] = "none"
+//	a["home"] = "hn"
+//	fmt.Println(a)
+//}
+
+//map使用
+func main() {
+
+	//var a map[string]string
+	//a = make(map[string]string,10) //不写空间默认为1
+	//a["name"] = "dw"
+	//fmt.Println(a)
+
+	//cities := make(map[string]string) //容量自增涨，无需定义
+	//cities["city1"] = "hz"
+	//cities["city2"] = "hz"
+	//cities["city3"] = "hz"
+	//cities["city4"] = "hz"
+	//fmt.Println(cities)
+
+	//name := map[string]string{
+	//	"name1" : "aa",
+	//	"name2" : "bb",
+	//}
+	//fmt.Println(name)
+
+}
---
+++ homework/main/main.go
@@ -0,0 +1,41 @@
+package main
+
+//p173
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	var arr [10]int
+	for i := 0; i < 10; i++ {
+		rand.Seed(time.Now().UnixNano())
+		var num int = rand.Intn(100)
+		fmt.Println(num)
+		time.Sleep(100 * time.Millisecond)
+		arr[i] = num + 1
+	}
+	fmt.Println(arr)
+	for index, value := range arr {
+		if value == 55 {
+			fmt.Println("yes", index)
+			break
+		}
+	}
+}
+
+//	fmt.Println(shuzu)
+//	for i := len(shuzu)-1 ; i >= 0;i-- {
+//		fmt.Println(shuzu[i])
+
+//for k:=len(arr)-1;k>0 ;k--  {
+//	for j:=0;j<len(arr)-1;j++{
+//		if (arr)[j] < (arr)[j+1]{
+//			var t int
+//			t = (arr)[j+1]
+//			(arr)[j+1] = (arr)[j]
+//			(arr)[j] = t
+//		}
+//	}
+//}
